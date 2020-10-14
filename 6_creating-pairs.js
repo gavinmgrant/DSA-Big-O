96932635f046ab684984258b47d73d0d23b1adb1
Gavin Grant <gavingrant@gmail.com>
2020-10-14 15:23:03 -0700

    feat(answer): answer to 6

--- 6_creating-pairs.js
+++ 6_creating-pairs.js
@@ -1,11 +1,11 @@
 function createPairs(arr) {
     let ticks = 0;
     for (let i = 0; i < arr.length; i++) {
-        ticks++;
         for(let j = i + 1; j < arr.length; j++) {
-            ticks++;
             console.log(arr[i] + ", " +  arr[j] );
+            ticks++;
         }
+        ticks++;
     }
     return { ticks: ticks };
 }
@@ -16,4 +16,4 @@ console.log(createPairs([1, 2, 3, 4, 5, 6, 7, 8]));
 console.log(createPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]));
 
 // What is the Big O of the following algorithm? Explain your answer
-// Answer: The Big O is
+// Answer: The Big O is O(n^2), polynomial time, since the algorithm requires 2 levels of looping.
